0edfdeda3e66f04e1f9d17220f4a5e81b4db9c34
sdgsnehal <sdgsnehal@gmail.com>
2023-04-07 18:19:50 +0530

    Nav Bar Added

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Client from "./Client";
+import Cta from "./Cta";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Client,
+  Cta,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
